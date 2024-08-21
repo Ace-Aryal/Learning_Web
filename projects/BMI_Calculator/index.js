20ccf456981155b52205f9d70a32fdbd80915574
Ace-Aryal <aryaldeelep@gmail.com>
2024-08-22 05:15:17 +0545

    update

--- projects/BMI_Calculator/index.js
+++ projects/BMI_Calculator/index.js
@@ -1,14 +1,18 @@
-let submitButtton = document.querySelector("#displayBMI")
-
-
-
-
-submitButtton.addEventListener("submit",()=>{
-  let height = document.querySelector ("#height").nodeValue;
-console.log(height)
-let weight = document.querySelector ("#weight").nodeValue
-  let BMI =( weight / height**2)
-  if BMI <= 18.4 {
-    updater.innerText = `Your BMI is : ${BMI} You're underweight `
+const  form = document.querySelector('form')
+// we are not taking height and weight here because those variables will take null value as the page loads rather we want to take value after the submission 
+form.addEventListener('submit',(e)=>{
+  e.preventDefault(); // to prevent submission to database
+  const height = parseInt(document.querySelector ('#height').value);
+  const weight = parseInt(document.querySelector ('#weight').value)
+  
+  
+  const BMI = (weight / (height/100)**2).toPrecision(4);
+  const display = document.querySelector ('#displayBMI')
+  
+  if ( height === '' || weight=== ''|| height <0 || height<0 || isNaN(height) || isNaN(weight )) {
+    display.innerText ='Please enter a valid entries'
   }
+  else {
+  display.innerText  = 'Your BMI is:' + BMI
 }
+})
