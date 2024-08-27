ef287a1f6770b6684d4782c367e94467964ccf08
Ace-Aryal <aryaldeelep@gmail.com>
2024-08-27 20:25:33 +0545

    update script.js

--- projects/Automatic_Background_Changer/script.js
+++ projects/Automatic_Background_Changer/script.js
@@ -1,25 +1,46 @@
+// selecting elements 
+
 const body = document.querySelector ("body")
 const start = document.querySelector ("#start")
 const stop = document.querySelector ("#stop")
 
 
-function Changer() {
-  // Tab to edit
-var Initializer = setInterval (function (){
-  let rgb = function random_rgba() {
-    var o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
-}
-let color = rgb()
 
-  body.style.backgroundColor = color
-},1000)
-stop.addEventListener('click' ,Stopper)
-function Stopper (){
-  clearInterval(Initializer)
-}
+// Random color hex value generation
+function randomColor () {
+  const hex ='0123456789ABCDEF' // string of hex
+  let color='#' // color 
+  for (var i = 0; i < 6; i++) {
+    color +=  hex[Math.floor(Math.random()*16)] // concatenation of color with random index hex string 6 timea
+  }
+  body.style.backgroundColor = color // changing bg color
 }
 
 
-start.addEventListener('click', Changer)
+
+
+// making starter global to use stoppee in global
+let starter;
+function startChangingColor (param) {
+     starter = setInterval( randomColor , 1000
+  )
+}
+
+
+  // passing random color fn into set interval 
+  function stopChangingColor(param) {
+    const stopper = clearInterval(starter)
+  }
+
+  
+  // start changing color
+  
+  start.addEventListener('click', startChangingColor)
+  
+    // stopping set interval stop changing color
+  stop.addEventListener('click', stopChangingColor)
+  
+  
+  
+  
 
