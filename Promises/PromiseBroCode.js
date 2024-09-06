62431acdd30d59a15471ba7e5928a924205c3f74
Ace-Aryal <aryaldeelep@gmail.com>
2024-09-06 11:16:07 +0545

    update PromiseBroCode.js

--- Promises/PromiseBroCode.js
+++ Promises/PromiseBroCode.js
@@ -1,3 +1,13 @@
+// synchronous  = Executes line by line consecutively in a sequential manner 
+//                             Code that waits for an operation to complete.
+
+// asynchronous = Allows multiple operations to be performed   concurrently without waiting. Doesn't block the execution  flow and allows the program to continue.
+// (I/O operations, network requests, fetching data)
+// Handled with: Callbacks, Promises, Async/Await
+
+
+
+
 // Do these chores in sequence
 
 //1. Walk Dog
