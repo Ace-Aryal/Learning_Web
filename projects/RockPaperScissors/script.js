51a8bfa5193d57c19d6f812258f8a3ed6ce9e58d
Ace-Aryal <aryaldeelep@gmail.com>
2024-09-13 20:54:55 +0545

    update

--- projects/RockPaperScissors/script.js
+++ projects/RockPaperScissors/script.js
@@ -16,6 +16,9 @@ const computerScoreD = document.querySelector ("#computerScoreD")
 
 
 function playerGame (playerChoice) {
+  resultDisplay.classList.remove("win" , "lose", "tie")
+  
+  
   const computerChoice = choices[Math.floor(Math.random()*3)]
  let result =""
   // Tab to edit
@@ -23,41 +26,39 @@ function playerGame (playerChoice) {
     result = "It's a tie";
     
     
-  } else {
-    switch (playerChoice) {
-      case "rock":result = (computerChoice ==="scissors")? 'You Win !':'You lose :(';
-      
-        // Tab to edit
-        break;
-        case 'scissors': result = (computerChoice ==="paper")?'You Win !':'You lose :(';
-          // Tab to edit
-          break;
-          case 'paper': result = (computerChoice==="rock")?"You Win !" :"You lose (:";
-            // Tab to edit
-            break;
-          
-        
-      
-      
-    }
-  }
-  if (playerChoice===computerChoice) {
-    
-    
-    
-  } else {
+  } 
+ else {
   switch (playerChoice) {
-      case "rock": (computerChoice ==="scissors")? playerScore++:computerScore++;
+      case "rock": { if (computerChoice ==="scissors") {
+        playerScore++
+        result = "You Win , Your rock beat computer's scissors" 
+      }
+      else {
+         result = "You lose , computer's paper beat your rock " 
+         computerScore++
+      }
+      }
       
-        // Tab to edit
-        break;
-        case 'scissors': (computerChoice ==="paper")? playerScore++:computerScore++;
-          // Tab to edit
+         break;
+        case 'scissors':  { if (computerChoice ==="paper") {
+          playerScore++
+        result = "You Win , Your scissors beat computer's paper" 
+      }
+      else {
+         result = "You lose , computer's rock beat your scissors " 
+         computerScore++
+      }
+      }
+      
           break;
-          case 'paper': (computerChoice==="rock")? playerScore++ :computerScore++;
-            // Tab to edit
-            break;
-          
+          case 'paper':  { if (computerChoice ==="rock") {playerScore++
+        result = "You Win , Your paper beat computer's rock" 
+      }
+      else {
+         result = "You lose , computer's scissor beat your paper " 
+         computerScore++
+      }
+      }
         
       
       
@@ -68,10 +69,26 @@ function playerGame (playerChoice) {
   computerChoiceDisplay.textContent =`Computer Choice :${computerChoice}`
   resultDisplay.textContent = result;
   
+  if(result === "You Win , Your paper beat computer's rock" || result ===  "You Win , Your scissors beat computer's paper" ||result === "You Win , Your rock beat computer's scissors")  { 
+  resultDisplay.classList.add("win")
+  }
+  
+  if (result === "You lose , computer's scissor beat your paper " ||result === "You lose , computer's rock beat your scissors " || result ==="You lose , computer's paper beat your rock "  ) {
+  resultDisplay.classList.add("lose")
+  } 
+  if
+  (result === "It's a tie" ){
+  resultDisplay.classList.add("tie")
+  }
+  
   playerScoreD.textContent= `Your Score : ${playerScore} `
+  playerScoreD.style.color = "limegreen"
 
 computerScoreD.textContent= `Computer Score : ${computerScore} `
+computerScoreD.style.color ="red"
+
 }
+
 
 
 playerRock.addEventListener('click',(e)=>{
