3541f8bc1debf8dfbf659ba71efe768fdc0af896
Ace-Aryal <aryaldeelep@gmail.com>
2024-09-14 13:31:16 +0545

    update

--- projects/compoundInterestCalculator/script.js
+++ projects/compoundInterestCalculator/script.js
@@ -14,16 +14,20 @@ submit.addEventListener("click",(e)=>{
   let principal = principalel.value
 let rate = rateel.value/100;
 let year = yearel.value 
-if (principal <0 || isNaN(principal) || rate >0 || isNaN(rate) || year > 0 || isNaN(year) ) {
+if (principal < 0 || isNaN(principal) || rate < 0 || isNaN(rate) || year < 0 || isNaN(year) ) {
   errorDisplay.innerHTML= "Enter Correct Input "
 }
 
 else {
   
 let total = (principal*((1+rate)**year)).toFixed(2)
-  resultDisplay.innerText = `$${total}`
+let formattedTotal = new Intl.NumberFormat().format(total)
+
+  resultDisplay.innerText = `$${formattedTotal}`
 }
-  
+rateel.value = null
+yearel.value = null
+principalel.value = null
 }
 )
 
