eac4d8d075be0cff85ad142c1159f692f0be5054
Ace-Aryal <aryaldeelep@gmail.com>
2024-09-21 21:12:25 +0545

    update

--- tips.js
+++ tips.js
@@ -1,2 +1,10 @@
 // Use guard classes if,if,ìf without those huge if ,else chains to make code cleaner 
 // only one responsibility for a function or class
+
+const num = 69_420_0.45
+console.log(num)
+
+// What's the output ?
+
+
+
