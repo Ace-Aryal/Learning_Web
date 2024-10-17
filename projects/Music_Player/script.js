2644c7a52e2b3c76ac44eb669d95a5f2908552f4
Ace-Aryal <aryaldeelep@gmail.com>
2024-10-17 20:30:22 +0545

    update

--- projects/Music_Player/script.js
+++ projects/Music_Player/script.js
@@ -1,9 +1,10 @@
 const faChevronDown = document.querySelector (".fa-chevron-down")
 const playlistArea = document.querySelector (".playlistArea")
 const searchBar = document.querySelector ("#searchBar")
-
+const sliderRange = document.querySelector ("#progress")
 faChevronDown.addEventListener("click",togglePlaylist)
 searchBar.addEventListener("input",displayPlaylist)
+sliderRange.addEventListener("input",displayRange)
 
 function togglePlaylist(e) {
 e.preventDefault()
@@ -12,6 +13,7 @@ faChevronDown.classList.toggle("fa-chevron-down")
 faChevronDown.classList.toggle("fa-times")
 }
 
+
 function displayPlaylist(param) {
   // Tab to edit
   playlistArea.classList.remove("displaying")
@@ -19,6 +21,13 @@ faChevronDown.classList.remove("fa-times")
   playlistArea.classList.add("displaying")
 faChevronDown.classList.remove("fa-chevron-down")
 faChevronDown.classList.add("fa-times")
+}
+
+function displayRange() {
+  // Tab to edit
+  const value = sliderRange.value
+  sliderRange.style.background= `linear-gradient(to right, #2887e3 ${value
+  }%, lightgray ${value}%)`
 }
 
 
